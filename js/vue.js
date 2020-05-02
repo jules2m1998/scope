6384e78cb0a6760b7e8e7b493ad98e4b03ade583
Jules Junior Meva'a <mevaajules9@gmail.com>
2020-05-03 00:18:19 +0100

    ajout des statistiques et de la liste des pays

--- js/vue.js
+++ js/vue.js
@@ -1,7 +1,42 @@
 new Vue({
     el: '#container',
+    vuetify: new Vuetify(),
     data: ({
         name: 'Jules',
-        search: 'Home'
-    })
+        search: '',
+        cases: null,
+        death: null,
+        recored: null,
+        all: null,
+
+    }),
+    mounted() {
+        axios.get("https://disease.sh/v2/all")
+            .then(response => {
+                this.cases = this.numberWithCommas(response.data.cases);
+                this.death = this.numberWithCommas(response.data.deaths);
+                this.recored = this.numberWithCommas(response.data.recovered)
+            });
+        axios.get("https://disease.sh/v2/countries")
+            .then(response => {
+                this.all = response.data
+            })
+    },
+    methods: {
+        numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        },
+        getPourcent(cases, recovered){
+            return Math.trunc( recovered/cases * 100);
+        },
+        getColor(pourcent){
+            if(pourcent <= 100 && pourcent >= 60){
+                return '#009688';
+            } else if(pourcent < 60 && pourcent >= 40){
+                return '#FF9800'
+            } else {
+                return '#F44336'
+            }
+        }
+    }
 });
